7a8a6ce890ac4aa522752a572b88453136c14834
fisketheater <100011901+fisketheater@users.noreply.github.com>
2022-02-19 13:28:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
  // just change these urls and the QR code will randomly select one of the to forward the user to. The current URLs are just placeholders.
-urls = ['https://colorado.edu/fiske', 'https://colorado.edu/sbo', 'https://colorado.edu']
+urls = ['https://cuboulder.qualtrics.com/jfe/form/SV_3IggIEY7MXHREqi']
 
 function redirect() {
     window.location.replace(urls[getRandomInt(urls.length)])
