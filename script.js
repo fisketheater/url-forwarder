e5c21fbbf671630d33ccfd8c49527c17c66ca80b
fisketheater <100011901+fisketheater@users.noreply.github.com>
2022-02-18 23:57:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
- // just change these urls and the QR code will randomly select one of the to forward the user to
+ // just change these urls and the QR code will randomly select one of the to forward the user to. The current URLs are just placeholders.
 urls = ['https://colorado.edu/fiske', 'https://colorado.edu/sbo', 'https://colorado.edu']
 
 function redirect() {
